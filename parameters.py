d244da2c15dca9e027c2b855d3521dd0970eb17d
Meatssauce <70827089+Meatssauce@users.noreply.github.com>
2021-08-24 19:58:19 +1000

    initial commit

---
+++ parameters.py
@@ -0,0 +1,10 @@
+request_headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,'
+              'application/signed-exchange;v=b3',
+    'Accept-Encoding': 'gzip',
+    'Accept-Language': 'en-US,en;q=0.9,es;q=0.8',
+    'Upgrade-Insecure-Requests': '1',
+    # 'Referer': 'https://www.google.com/'
+}
+sources = {'tvfy': 'https://theyvoteforyou.org.au/', 'pa': 'https://postcodes-australia.com/'}
