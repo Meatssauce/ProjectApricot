3e9bcdd197c41fe7c36db80a993f5fd4bb44299b
Meatssauce <70827089+Meatssauce@users.noreply.github.com>
2021-09-12 02:14:37 +1000

    added more websites

--- main.py
+++ main.py
@@ -20,3 +20,11 @@
 # prefer a more liberal or authoritarian approach to enforcing their worldview
 # in addition: economic spectrum (horizontal)
 # determined based on specific voting history (economic policies)
+
+# some sources for structured data
+# https://www.aph.gov.au/Senators_and_Members/
+# https://www.openaustralia.org.au/
+# https://theyvoteforyou.org.au/
+
+# Structure unclear
+# https://parlinfo.aph.gov.au/parlInfo/search/display/display.w3p;db=CHAMBER;id=chamber%2Fhansards%2F395f68fa-06e3-4162-a1b9-1ef89b58cd19%2F0088;query=Id%3A%22chamber%2Fhansards%2F395f68fa-06e3-4162-a1b9-1ef89b58cd19%2F0126%22
