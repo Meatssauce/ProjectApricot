609b9363f1bc1272bbd66bcd2927471b43634f19
Meatssauce <70827089+Meatssauce@users.noreply.github.com>
2021-09-28 22:28:22 +1000

    saves to separate csv file instead

--- add_tweet_based_embeddings.py
+++ add_tweet_based_embeddings.py
@@ -8,22 +8,23 @@ from sentence_transformers import SentenceTransformer
 
 # Load model and data
 model = SentenceTransformer('all-mpnet-base-v2')  # SOTA for sentence embedding according to official docs
-politicians = pd.read_csv('dataset/au_parliament_members_data.csv')
+politicians = pd.read_csv('dataset/au_parliament_members_data.csv', index_col='Name')
 
-# Compute tweet-based semantic embedding for each politician
+# Compute tweet-based semantic embedding for each politician indexed by politician name
 politician_embeddings = []
-for name, handle in tqdm(zip(politicians['Name'], politicians['Twitter Handle']), total=len(politicians)):
+for handle in tqdm(politicians['Twitter Handle']):
     if handle is np.nan:
-        politician_embeddings.append(np.nan)
-
+        politician_embeddings.append([])
     else:
         # Load tweets by the politician
         tweets_data = []
         with open(f'dataset/tweets/{handle}.json', 'r') as f:
             for line in f:
                 tweets_data.append(json.loads(line))
+
+        # Append empty embedding if tweets have no content todo: investigate reason for no content in tweets df
         if 'content' not in (tweets := pd.DataFrame.from_records(tweets_data)).columns:
-            politician_embeddings.append(np.nan)
+            politician_embeddings.append([])
             continue
 
         # Remove urls and then hashtags from tweets
@@ -33,6 +34,7 @@ for name, handle in tqdm(zip(politicians['Name'], politicians['Twitter Handle'])
         # Produce a 1D embedding by taking the mean
         embeddings = model.encode(tweets.content.to_list())
         politician_embeddings.append(embeddings.mean(axis=0))
-politicians['tweetEmbedding'] = politician_embeddings
+politician_embeddings = pd.DataFrame(politician_embeddings, index=politicians.index)
 
-politicians.to_csv('dataset/au_parliament_members_data.csv', index=False)
+# Save embeddings
+politician_embeddings.to_csv('dataset/tweet_based_embeddings.csv')
