ccf21f7b72cd24894259fc2101fb3e9896a7619e
Meatssauce <70827089+Meatssauce@users.noreply.github.com>
2021-09-28 21:32:14 +1000

    changed embedding column name

--- add_tweet_based_embeddings.py
+++ add_tweet_based_embeddings.py
@@ -33,6 +33,6 @@ for name, handle in tqdm(zip(politicians['Name'], politicians['Twitter Handle'])
         # Produce a 1D embedding by taking the mean
         embeddings = model.encode(tweets.content.to_list())
         politician_embeddings.append(embeddings.mean(axis=0))
-politicians['embedding'] = politician_embeddings
+politicians['tweetEmbedding'] = politician_embeddings
 
 politicians.to_csv('dataset/au_parliament_members_data.csv', index=False)
