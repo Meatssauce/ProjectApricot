29b5fe6db36a8d1ccbb1a025c180edeae64090e2
Meatssauce <70827089+Meatssauce@users.noreply.github.com>
2021-11-27 22:09:03 +1100

    implemented multi-threaded webhose api call without offical package

--- data_io/api.py
+++ data_io/api.py
@@ -2,12 +2,12 @@ import warnings
 from enum import Enum
 import datetime
 from math import ceil
-import webhoseio
 from tqdm import tqdm
 from multiprocessing import Process, Queue
 from collections import Iterator
 from threading import Lock
 import time
+import requests
 
 PRIVATE_API_KEY = "12fede3e-49db-40a0-adc9-62d69ba00005"
 
@@ -34,11 +34,11 @@ class DataType(Enum):
     REVIEWS = "reviewFilter"
 
 
-class Format(Enum):
-    JSON = 'json'
-    XML = 'xml'
-    RSS = 'rss'
-    EXCEL = 'excel'
+# class Format(Enum):
+#     JSON = 'json'
+#     XML = 'xml'
+#     RSS = 'rss'
+#     EXCEL = 'excel'
 
 
 class SortingPolicy(Enum):
@@ -98,173 +98,171 @@ class RateLimiter(Iterator):
             self.next_yield_time = time.perf_counter() + self.interval
 
 
-def _query_writer(queue: Queue, call_api, end_point_str: str, param_dict: dict):
-    result_batch = call_api(end_point_str, param_dict)
-    api_rate_limiter = RateLimiter(param_dict['cool_down_duration'])
+class WebhoseIO:
+    def __init__(self, token=None):
+        self.next_call = None
+        self.session = requests.Session()
+        self.token = token
+        self.max_batches = None
+        self.cool_down_duration = None
+        self.params = None
+        self._end_of_queue = 'DONE'
 
-    batch_size = param_dict['size']
-    initial_batch_size = len(result_batch['docs'])
-    expected_total_results = min(param_dict['max_results'], result_batch['totalResults'])
+    def set_params(self,
+                   keywords: str = '',
+                   filters: dict = None,
+                   time_since: datetime.timedelta = datetime.timedelta(days=3),
+                   sort_: str = SortingPolicy.get_default().value,
+                   batch_size: int = 10,
+                   max_batches: int = float('inf'),
+                   cool_down_duration: datetime.timedelta = datetime.timedelta(seconds=1),
+                   **kwargs) -> None:
+        """
+        Get parameters for API query. See https://docs.webz.io/reference#news-blogs-discussions-api-overview for
+        details on parameters and filters.
 
-    for results_so_far in tqdm(range(initial_batch_size, expected_total_results + 1, batch_size),
-                               initial=initial_batch_size, total=expected_total_results,
-                               unit='article', unit_scale=batch_size):
-        queue.put(result_batch)
+        :param keywords: input to search engine - e.g. ("Donald Trump" OR president) -Obama
+        :param filters: speical parameters used to filter search results
+        :param time_since: time since earliest possible result
+        :param sort_: metric by which search results are sorted
+        :param batch_size: number of results returned each API call
+        :param max_batches: maximum number of batches of results to receive from API (use a small value to
+        limit API calls)
+        :param cool_down_duration: amount of time between each successive API call (default - 1 second)
+        :return: None
+        """
 
-        if results_so_far + batch_size > expected_total_results + 1:
-            break
-        else:
-            next(api_rate_limiter)
-            result_batch = webhoseio.get_next()
-    queue.put('DONE')
+        def parse_filters(filters: dict) -> str:
+            substrings = [f'{k}:{v}' for k, v in filters.items()]
+            return ' '.join(substrings)
 
+        # Show warnings
+        if keywords and 'category' in filters:
+            warnings.warn('Using non-empty keywords with category may cause relevant results to be missing.',
+                          ParamsWarning)
+        if sort_ != SortingPolicy.get_default().value:
+            warnings.warn('\nSorting by any sort parameter value other than CRAWL_DATE (default) may result in missing'
+                          ' important posts. \nIf data integrity is important, stick with the default recommended sort'
+                          ' parameter value of crawl date, and consume the data as a stream.', ParamsWarning)
 
-def _wrapped_query(end_point_str: str, param_dict=None):
-    """A more user-friendly alternative of webhoseio's official query method that shows warnings and uses
-    multithreading to maximise speed."""
-    # Basic input validation - not complete
-    if end_point_str == DataType.NEWS_BLOGS_AND_FORMS.value:
-        if 'category:' in param_dict['q']:
-            raise ParamsError("Cannot query NEWS_BLOGS_AND_FORMS with parameter 'category'.")
-        if 'site.country:' in param_dict['q'] or 'item.country:' in param_dict['q']:
-            raise ParamsError("Invalid country filter for querying NEWS_BLOGS_AND_FORMS.")
-    elif end_point_str == DataType.ENRICHED_NEWS.value:
-        if 'thread.country:' in param_dict['q'] or 'item.country:' in param_dict['q']:
-            raise ParamsError("Invalid country filter for querying ENRICHED_NEWS.")
-    elif end_point_str == DataType.REVIEWS.value:
-        if 'thread.country:' in param_dict['q'] or 'site.country:' in param_dict['q']:
-            raise ParamsError("Invalid country filter for querying ENRICHED_NEWS.")
-    warnings.warn("\nThe webhose news API does not return warnings as expected. This method only provides minimal "
-                  "input validation. \nPlease check dashboard on API website for error logs if you keep getting 0 "
-                  "results.")
+        # Parse query string
+        query_string = ''
+        if keywords:
+            query_string += f'{keywords} '
+        if filters:
+            query_string += parse_filters(filters)
 
-    results_queue = Queue()
-    docs = []
+        self.max_batches = max_batches
+        self.cool_down_duration = cool_down_duration
+        self.params = {
+            'q': query_string,
+            'time_since': int((datetime.datetime.now() - abs(time_since)).timestamp()),
+            'sort': sort_,
+            'size': batch_size,
+            **kwargs
+        }
 
-    writer_process = Process(target=_query_writer, args=(results_queue, _query, end_point_str, param_dict))
-    writer_process.start()
+    def query(self, end_point_str: str) -> list:
+        """A more user-friendly alternative of webhoseio's official query method that shows warnings and uses
+        multithreading to maximise speed."""
+        # Basic input validation - not complete
+        if end_point_str == DataType.NEWS_BLOGS_AND_FORMS.value:
+            if 'category:' in self.params['q']:
+                raise ParamsError("Cannot query NEWS_BLOGS_AND_FORMS with parameter 'category'.")
+            if 'site.country:' in self.params['q'] or 'item.country:' in self.params['q']:
+                raise ParamsError("Invalid country filter for querying NEWS_BLOGS_AND_FORMS.")
+        elif end_point_str == DataType.ENRICHED_NEWS.value:
+            if 'thread.country:' in self.params['q'] or 'item.country:' in self.params['q']:
+                raise ParamsError("Invalid country filter for querying ENRICHED_NEWS.")
+        elif end_point_str == DataType.REVIEWS.value:
+            if 'thread.country:' in self.params['q'] or 'site.country:' in self.params['q']:
+                raise ParamsError("Invalid country filter for querying ENRICHED_NEWS.")
+        warnings.warn("\nThe webhose news API does not return warnings as expected. This method only provides minimal "
+                      "input validation. \nPlease check dashboard on API website for error logs if you keep getting 0 "
+                      "results.")
 
-    # Process api call results
-    while True:
-        results = results_queue.get()
-        if results == 'DONE':
-            break
-        else:
-            docs += results['docs']
-    writer_process.join()
+        response_queue = Queue()
 
-    return docs
+        # Write API response to queue in another thread
+        writer_process = Process(target=self._response_writer, args=(response_queue, end_point_str))
+        writer_process.start()
 
+        # Process API responses added to the queue in main thread
+        if (response := response_queue.get()) == self._end_of_queue:
+            return []
 
-# def _overwrite_query():
-#     """Overwrite official query method of webhoseio with a more user-friendly alternative that also provides warnings
-#     and input parameters validation."""
-#
-#
-#     query = webhoseio.query
-#     webhoseio.query = _wrapped_query
+        docs = response['docs']
+        batch_size = self.params['size']
+        num_batches_to_read = min(self.max_batches, ceil(response['totalResults'] / batch_size))
 
+        for _ in tqdm(range(1, num_batches_to_read), initial=1, total=num_batches_to_read, unit='batch'):
+            if (response := response_queue.get()) == self._end_of_queue:
+                break
+            else:
+                docs += response['docs']
 
-def get_params(keywords: str = '',
-               filters: dict = None,
-               ts: int = None,
-               sort_: str = SortingPolicy.get_default().value,
-               size: int = 10,
-               max_results: int = float('inf'),
-               cool_down_duration: datetime.timedelta = datetime.timedelta(seconds=1),
-               format_: str = Format.JSON,
-               warnings_: bool = False) -> dict:
-    """
-    Get parameters for API query.
+        writer_process.join()
 
-    :param keywords: Input to search engine - e.g. ("Donald Trump" OR president) -Obama
-    :param filters: dictionary containing filters - see webhoseio documentation for usage
-    :param ts: time of earliest result in unix time (13 digits)
-    :param sort_: how results are sorted - Note: Sorting by any sort parameter value other than
-    crawl date (default) may result in missing important posts. If data integrity is important,
-    stick with the default recommended sort parameter value of crawl date, and consume the data as a stream.
-    :param size: number of articles returned per batch
-    :param max_results: maximum number of results to receive from API (use a small value to limit API calls)
-    :param cool_down_duration: amount of time between each API call (default - 1 second)
-    :param format_: output result in this format - integer between 2-100 inclusive
-    :param warnings_: show warnings via a warnings object in output
-    :return: dictionary containing valid parameters for the API
-    """
+        return docs
 
-    def parse_filters(filters: dict) -> str:
-        substrings = [f'{k}:{v}' for k, v in filters.items()]
-        return ' '.join(substrings)
+    def _get_next(self):
+        return self._query(self.next_call[1:])
 
-    # Show warnings
-    if keywords and 'category' in filters:
-        warnings.warn('Using non-empty keywords with category may cause relevant results to be missing.',
-                      ParamsWarning)
-    if sort_ != SortingPolicy.get_default().value:
-        warnings.warn('\nSorting by any sort parameter value other than CRAWL_DATE (default) may result in missing'
-                      ' important posts. \nIf data integrity is important, stick with the default recommended sort'
-                      ' parameter value of crawl date, and consume the data as a stream.', ParamsWarning)
+    def _query(self, end_point_str, param_dict=None):
+        if param_dict is not None:
+            param_dict.update({"token": self.token})
+            param_dict.update({"format": "json"})
 
-    # Parse query string
-    query_string = ''
-    if keywords:
-        query_string += f'{keywords} '
-    if filters:
-        query_string += parse_filters(filters)
+        response = self.session.get("http://webhose.io/" + end_point_str, params=param_dict)
+        if response.status_code != 200:
+            raise Exception(response.text)
 
-    # Set default time since to 3 lookback_window ago
-    if not ts:
-        ts = to_timestamp(days_ago=3)
+        _output = response.json()
+        self.next_call = _output['next']
+        return _output
 
-    return {
-        'q': query_string,
-        'ts': ts,
-        'sort': sort_,
-        'size': size,
-        'max_results': max_results,
-        'cool_down_duration': cool_down_duration,
-        'format': format_,
-        'warnings': warnings_,
-    }
+    def _response_writer(self, queue: Queue, end_point_str: str):
+        response = None
+        api_rate_limiter = RateLimiter(self.cool_down_duration)
 
+        for i in range(self.max_batches):
+            response = self._get_next() if response else self._query(end_point_str, self.params)
+            queue.put(response)
 
-def to_timestamp(days_ago: int) -> int:
-    proper_time = datetime.datetime.now() - datetime.timedelta(days=days_ago)
-    return int(proper_time.timestamp())
+            if response['moreResultsAvailable'] == 0:
+                break
+            else:
+                next(api_rate_limiter)
+        queue.put(self._end_of_queue)
 
 
-# webhoseio = DummyWebhoseio()
-
-# Overwrite webhoseio.query with wrapper
-if webhoseio.query is not _wrapped_query:
-    _query = webhoseio.query
-    webhoseio.query = _wrapped_query
+# def to_timestamp(days_ago: int) -> int:
+#     proper_time = datetime.datetime.now() - datetime.timedelta(days=days_ago)
+#     return int(proper_time.timestamp())
 
 
 def main():
     # Test only
-    # import webhoseio
+    webhoseio = WebhoseIO(token=PRIVATE_API_KEY)
 
-    webhoseio.config(token=PRIVATE_API_KEY)
-    query_params = get_params(
+    webhoseio.set_params(
         # keywords="\"Mark Dreyfus\"",
         filters={
             'language': 'english',
             'category': 'politics',
             'site.country': 'AU'
         },
-        ts=to_timestamp(days_ago=3),
-        size=100,
-        # max_results=50,
-        warnings_=True,
+        time_since=datetime.timedelta(days=3),
+        batch_size=100,
+        max_batches=3,
     )
-
-    output = webhoseio.query(DataType.ENRICHED_NEWS.value, query_params)
+    output = webhoseio.query(DataType.ENRICHED_NEWS.value)
     print(output)
     # print(output['posts'][0]['text'])  # Print the text of the first post
     # print(output['posts'][0]['published'])  # Print the text of the first post publication date
     #
     # # Get the next batch of posts
-    # output = webhoseio.get_next()
+    # output = webhoseio._get_next()
     # if output['posts']:
     #     print(output['posts'][0]['thread']['site'])  # Print the site of the first post
     # else:
